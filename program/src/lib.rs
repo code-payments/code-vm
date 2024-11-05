db506dc7568c34ecf6ebdfb3cef12e5d232e2c1b
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2024-11-05 00:27:18 -0500

    following the ORE team here

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -3,7 +3,12 @@ mod instruction;
 use instruction::*;
 
 use code_vm_api::prelude::*;
+use solana_include_idl::{include_idl, parse::IdlType};
 use steel::*;
+
+// Include the compressed IDL in an ELF section on the program binary.
+// Ref: https://github.com/regolith-labs/ore/pull/105
+include_idl!(IdlType::Codama, concat!(env!("OUT_DIR"), "/codama.idl.zip"));
 
 pub fn process_instruction(
     program_id: &Pubkey,
