b2673e237afa6c920162234e14abacf5e347890a
zfedoran <zelimir.fedoran@gmail.com>
2025-01-29 13:08:29 -0500

    correct logic when source and dest are the same (#11)

--- program/src/opcode/transfer.rs
+++ program/src/opcode/transfer.rs
@@ -100,13 +100,23 @@ pub fn process_transfer(
         hash.as_ref(),
     )?;
 
-    src_vta.balance = src_vta.balance
-        .checked_sub(args.amount)
-        .ok_or(ProgramError::ArithmeticOverflow)?;
+    if src_vta.balance < args.amount {
+        return Err(ProgramError::InsufficientFunds);
+    }
 
-    dst_vta.balance = dst_vta.balance
-        .checked_add(args.amount)
-        .ok_or(ProgramError::ArithmeticOverflow)?;
+    // If the source and destination accounts are the same, then we don't need
+    // to do anything.
+
+    let is_same_account = src_mem == dst_mem && src_index == dst_index;
+    if !is_same_account {
+        src_vta.balance = src_vta.balance
+            .checked_sub(args.amount)
+            .ok_or(ProgramError::ArithmeticOverflow)?;
+
+        dst_vta.balance = dst_vta.balance
+            .checked_add(args.amount)
+            .ok_or(ProgramError::ArithmeticOverflow)?;
+    }
 
     vdn.value = vm.get_current_poh();
 
