de44e148b51b2a34c66e6fc625a1f7e0340cfbcf
jeffyanta <jeff@flipcash.com>
2025-12-16 12:12:38 -0500

    Update security txt

--- program/src/security.rs
+++ program/src/security.rs
@@ -3,11 +3,10 @@ use {solana_security_txt::security_txt};
 
 #[cfg(not(feature = "no-entrypoint"))]
 security_txt! {
-    name: "code-vm",
-    project_url: "https://getcode.com",
-    contacts: "email:security@getcode.com,email:contact@getcode.com",
+    name: "Code VM",
+    project_url: "https://flipcash.com",
+    contacts: "email:security@flipcash.com",
     policy: "https://github.com/code-payments/code-vm/blob/main/SECURITY.md",
-    preferred_languages: "en",
     source_code: "https://github.com/code-payments/code-vm",
     auditors: "OtterSec"
 }
